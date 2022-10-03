1f6ca462db9bb696be93cebca8c8bdab874459c0
MatteoMancin <matteomancin2304@gmail.com>
2022-10-03 08:49:45 +0200

    Initial commit

---
+++ prima_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import homepage, welcome, lista, chiSiamo
+
+app_name = "prima_app"
+urlpatterns = [
+    path('', homepage, name="homepage"),
+    path('welcome', welcome, name="welcome"),  
+    path('lista/', lista, name="lista"),
+    path('chi_siamo/', chiSiamo, name="chiSiamo"),
+]
