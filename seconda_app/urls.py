e7c393f67336dab4a3644a39091756d40290c29c
MatteoMancin <matteomancin2304@gmail.com>
2022-10-13 12:36:00 +0200

    update
    ciao

--- seconda_app/urls.py
+++ seconda_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import es_if
+from .views import es_if, if_else_elif, menu, es_for
 app_name = "seconda_app"
 urlpatterns = [
-    path("es_if", es_if, name="es_if")
+    path("", menu, name="menu"),
+    path("es_if", es_if, name="es_if"),
+    path("if_else_elif", if_else_elif, name="if_else_elif"),
+    path("es_for", es_for, name="es_for")
 
 ]
--- seconda_app/views.py
+++ seconda_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import datetime
 
 def es_if(request):
     context = {
@@ -7,4 +8,22 @@ def es_if(request):
         "var3": 300
     }
     return render(request, "es_if.html", context)
+
+def if_else_elif(request):
+    context = {
+        "var1": 100,
+        "var2": 100.0,
+        "var3": 100.50,
+    }
+    return render(request, "if_else_elif.html", context)
 # Create your views here.
+
+def menu(request):
+    return render(request, "menu.html")
+
+def es_for(request):
+    context = {
+        "list1": [1, datetime.date(2022, 10, 13), "Do not give up!"],
+        "list2": [1, datetime.date(2022, 10, 13), "Do not give up!"]
+    }
+    return render(request, "es_for.html", context)
